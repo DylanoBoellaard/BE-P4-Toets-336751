0607529f3827abb77711e5681460daaf3c7ca6b7
DylanoBoellaard <336751@student.mboutrecht.nl>
2023-06-01 14:37:53 +0200

    Added basic styling to examinator- and homepage

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,5 @@
 <?php require_once 'C:\Users\Gebruiker\Documents\School Utrecht\Backend\Leerjaar 1\Periode 3\MVC-Framework\app\views\includes\header.php'; ?>
-
+    <link rel="stylesheet" href="../../../public/css/homepageGlobal.css">
     <h2><?= $data['title']; ?></h2>
     <a href="<?= URLROOT; ?>/examinator/index">Overzicht afgenomen examens examinatoren</a>
 
