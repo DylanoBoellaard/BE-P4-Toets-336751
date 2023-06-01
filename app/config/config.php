bbdda05f906f84edc5ccd06b04cb7603888bbdd6
DylanoBoellaard <336751@student.mboutrecht.nl>
2023-06-01 13:56:46 +0200

    Added working create DB script

--- app/config/config.php
+++ app/config/config.php
@@ -3,6 +3,6 @@
 define('DB_HOST', 'localhost');
 define('DB_USER','root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvc-2209b-P4-Ljr1');
-define('URLROOT', 'http://ljr1-p4-mvc-opdrachten');
+define('DB_NAME', 'mvc-2209b-P4-Toets');
+define('URLROOT', 'http://be-p4-toets');
 define('APPROOT', dirname(dirname(__FILE__)));
---
+++ app/db/createScript.sql
@@ -0,0 +1,150 @@
+-- Step: 01
+-- Goal: Create a new database mvc-2209b-P4-Toets
+-- **********************************************************************************
+-- Version       Date:           Author:                     Description:
+-- *******       **********      ****************            ******************
+-- 01            04-04-2023      Dylano Boellaard            New
+-- **********************************************************************************/
+
+-- Check if the database exists
+DROP DATABASE IF EXISTS `mvc-2209b-P4-Toets`;
+
+-- Create a new Database
+CREATE DATABASE IF NOT EXISTS `mvc-2209b-P4-Toets`;
+
+-- Use database mvc-2209b-P4-Toets
+Use `mvc-2209b-P4-Toets`;
+
+-- Step: 02
+-- Goal: Create a new table Examen
+-- **********************************************************************************
+-- Version       Date:           Author:                     Description:
+-- *******       **********      ****************            ******************
+-- 01            04-04-2023      Dylano Boellaard            New
+-- **********************************************************************************/
+
+-- Drop table Examen
+DROP TABLE IF EXISTS Examen;
+
+CREATE TABLE IF NOT EXISTS Examen
+(
+    Id                  TINYINT         UNSIGNED        NOT NULL    AUTO_INCREMENT
+   ,StudentId           VARCHAR(6)                      NOT NULL
+   ,Rijschool           VARCHAR(50)                     NOT NULL
+   ,Stad                VARCHAR(50)                     NOT NULL
+   ,Rijbewijscategorie  VARCHAR(10)                     NOT NULL
+   ,Datum               DATE                            NULL
+   ,Uitslag             VARCHAR(25)                     NULL
+   ,IsActief            BIT                             NOT NULL    DEFAULT 1
+   ,Opmerkingen         VARCHAR(250)                        NULL    DEFAULT NULL
+   ,DatumAangemaakt     DateTime(6)                     NOT NULL
+   ,DatumGewijzigd      DateTime(6)                     NOT NULL
+
+   ,CONSTRAINT      PK_Examen_Id   PRIMARY KEY CLUSTERED(Id)
+) ENGINE=InnoDB;
+
+
+-- Step: 03
+-- Goal: Fill table Examen with data
+-- **********************************************************************************
+-- Version       Date:           Author:                     Description:
+-- *******       **********      ****************            ******************
+-- 01            04-04-2023      Dylano Boellaard            New
+-- **********************************************************************************/
+
+INSERT INTO Examen
+(
+     StudentId
+    ,Rijschool
+    ,Stad
+    ,Rijbewijscategorie
+    ,Datum
+    ,Uitslag
+    ,IsActief
+    ,Opmerkingen
+    ,DatumAangemaakt
+    ,DatumGewijzigd
+)
+VALUES
+     ('100234', 'VolGasVooruit', 'Rotterdam', 'B', 2023-04-03, 'Geslaagd', 1, NULL, SYSDATE(6), SYSDATE(6))
+     ,('123432', 'InZijnVierDoorDeBocht', 'Sliedrecht', 'C', 2023-03-01, 'Geslaagd', 1, NULL, SYSDATE(6), SYSDATE(6))
+     ,('103234', 'LinksomRechtsom', 'Dordrecht', 'D', 2023-05-15, 'Geslaagd', 1, NULL, SYSDATE(6), SYSDATE(6))
+     ,('106452', 'OpDeVluchtStrook', 'Zwijndrecht', 'AM', 2023-05-15, 'Gezakt', 1, NULL, SYSDATE(6), SYSDATE(6))
+     ,('104546', 'RechtDoorEnGaan', 'Rotterdam', 'B', 2023-04-17, 'Gezakt', 1, NULL, SYSDATE(6), SYSDATE(6))
+     ,('100333', 'AltijdGeslaagd', 'Dordrecht', 'B', 2023-05-12, 'Geslaagd', 1, NULL, SYSDATE(6), SYSDATE(6))
+     ,('124444', 'RijlesVoorJou', 'Rotterdam', 'B', 2023-04-12, 'Geslaagd', 1, NULL, SYSDATE(6), SYSDATE(6));
+    
+
+
+-- Stap 04, Create Examinator table
+CREATE TABLE IF NOT EXISTS Examinator
+(
+    Id                      TINYINT         UNSIGNED        NOT NULL    AUTO_INCREMENT
+   ,Voornaam                VARCHAR(50)                     NOT NULL
+   ,Tussenvoegsel           VARCHAR(25)                     NULL
+   ,Achternaam              VARCHAR(50)                     NOT NULL
+   ,Mobiel                  VARCHAR(15)                     NOT NULL
+   ,IsActief                BIT                             NOT NULL    DEFAULT 1
+   ,Opmerkingen             VARCHAR(250)                        NULL    DEFAULT NULL
+   ,DatumAangemaakt         DateTime(6)                     NOT NULL
+   ,DatumGewijzigd          DateTime(6)                     NOT NULL
+
+   ,CONSTRAINT      PK_Examinator_Id   PRIMARY KEY CLUSTERED(Id)
+) ENGINE=InnoDB;
+
+
+-- Stap 05, Fill Examinator table with data
+INSERT INTO Examinator
+(
+    Voornaam
+    ,Tussenvoegsel
+    ,Achternaam
+    ,Mobiel
+    ,IsActief
+    ,Opmerkingen
+    ,DatumAangemaakt
+    ,DatumGewijzigd
+)
+VALUES
+('Manuel', 'Van', 'Meekeren', '06-28493823', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Lissette', 'Den', 'Dongen', '06-24383299', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Jurswailly', '', 'Luciano', '06-48293846', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Sharida', '', 'Tetehuka', '06-48393823', 1, NULL, SYSDATE(6), SYSDATE(6))
+,('Naswha', '', 'Salawi', '06-34291219', 1, NULL, SYSDATE(6), SYSDATE(6));
+
+
+-- Stap 06, Create ExamenPerExaminator table
+CREATE TABLE IF NOT EXISTS ExamenPerExaminator
+(
+    Id                      TINYINT         UNSIGNED        NOT NULL    AUTO_INCREMENT
+   ,ExamenId                TINYINT         UNSIGNED        NOT NULL
+   ,ExaminatorId            TINYINT         UNSIGNED        NOT NULL
+   ,IsActief                BIT                             NOT NULL    DEFAULT 1
+   ,Opmerkingen             VARCHAR(250)                    NULL        DEFAULT NULL
+   ,DatumAangemaakt         DateTime(6)                     NOT NULL
+   ,DatumGewijzigd          DateTime(6)                     NOT NULL
+
+   ,CONSTRAINT      PK_ExamenPerExaminator_Id      PRIMARY KEY CLUSTERED(Id)
+   ,CONSTRAINT      FK_ExamenPerExaminator_ExamenId_Examen_Id   FOREIGN KEY (ExamenId) REFERENCES Examen(Id)
+   ,CONSTRAINT      FK_ExamenPerExaminator_ExaminatorId_Examinator_Id   FOREIGN KEY (ExaminatorId) REFERENCES Examinator(Id)
+) ENGINE=InnoDB;
+
+
+-- Stap 07, Fill ExamenPerExaminator table with data
+INSERT INTO ExamenPerExaminator
+(
+    ExamenId
+    ,ExaminatorId
+    ,IsActief
+    ,Opmerkingen
+    ,DatumAangemaakt
+    ,DatumGewijzigd
+)
+VALUES
+(1, 3, 1, NULL, SYSDATE(6), SYSDATE(6))
+,(3, 3, 1, NULL, SYSDATE(6), SYSDATE(6))
+,(2, 2, 1, NULL, SYSDATE(6), SYSDATE(6))
+,(4, 1, 1, NULL, SYSDATE(6), SYSDATE(6))
+,(7, 3, 1, NULL, SYSDATE(6), SYSDATE(6))
+,(6, 2, 1, NULL, SYSDATE(6), SYSDATE(6))
+,(5, 4, 1, NULL, SYSDATE(6), SYSDATE(6));
